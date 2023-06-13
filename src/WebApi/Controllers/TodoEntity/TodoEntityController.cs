7369110bd8c8dd74b910f5a8363718dfd0a653d7
nrtadd <whitesnowtot@gmail.com>
2023-06-13 15:59:01 +0300

    Update StatusCodes controller

--- src/WebApi/Controllers/TodoEntity/TodoEntityController.cs
+++ src/WebApi/Controllers/TodoEntity/TodoEntityController.cs
@@ -24,6 +24,7 @@ namespace WebApi.Controllers.TodoEntity
         [HttpGet("id")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<GetTodoEntityVm>> Get(Guid id)
         {
             var query = new GetTodoEntityQuery
@@ -37,6 +38,7 @@ namespace WebApi.Controllers.TodoEntity
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<GetListTodoEntityList>> GetAll()
         {
             var query = new GetListTodoEntityQuery
--- src/WebApi/Controllers/TodoList/TodoListCotroller.cs
+++ src/WebApi/Controllers/TodoList/TodoListCotroller.cs
@@ -23,6 +23,7 @@ namespace WebApi.Controllers.TodoList
         [HttpGet("id")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<GetTodoListVm>> Get(Guid id)
         {
             var query = new GetTodoListQuery
@@ -36,6 +37,7 @@ namespace WebApi.Controllers.TodoList
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<GetTodoCatalog>> GetAll()
         {
             var query = new GetTodoCatalogQuery
